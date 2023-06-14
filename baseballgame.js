9612016c04ce66535a5d76e2410eddca4b6d4c51
myeongjin <seomyeongjin99@gmail.com>
2023-06-14 14:47:52 +0900

    지현주, 서명진 baseballgame 최종

--- baseballgame.js
+++ baseballgame.js
@@ -16,29 +16,27 @@ function randomNum() {
         }
         
     }
-    
     return num.join("")
 }
 
 // 게임결과 반환
-function gameResult() {
+function gameResult(pcInput,userInput) {
     let strike = 0;
     let ball = 0;
-    let count = 0;
     let pc = randomNum();
     
-    console.log('pc',pc)
+    // console.log('pc',pc)
     rl.on("line", function(userInput) {  
     //숫자의 값과 위치가 모두 일치하면 S
-    for(let i=0; i < 3; i++){
-        for(let j = 0; j < 3 ; j++){
+    for(let i=0; i < 3; i++){ // 0 
+        for(let j = 0; j < 3 ; j++){ // 0
             // pc.charAt(i)===userInput.charAt(j) && i === j
             // ? strike++
             // : ball++
 
             // if(pcInput[i]===userInput[j]){
-            if(pc.charAt(i)===userInput.charAt(j)){
-                if(i === j){
+            if(pc.charAt(i)===userInput.charAt(j)){ // 숫자 자체가 같은지 확인 
+                if(i === j){ // 위치도 같은지 확인 
                     strike++
                 }else{
                     ball++
@@ -46,9 +44,12 @@ function gameResult() {
             }
         }
     }
-    count++
+    
+    // let count = 1;
+    count++;
     if(strike===3){
-        console.log(`${ball}B${strike}S`)
+        //0B3S
+        console.log(`${strike}S`)
         console.log(`${count}번 만에 맞히셨습니다.`)
         rl.close();
     } else if(ball === 3) {
@@ -60,7 +61,9 @@ function gameResult() {
         console.log(`${ball}B${strike}S`)
     }
 });
-}
+};
+
+
 // - 기회는 무제한이며, n번의 시도 후에 맞췄는지 기록됩니다. = n 초깃값 1, 누적 증가
 // - 숫자 3개를 모두 맞춘 경우, 게임을 종료합니다. = 3S 일 때 종료
 
